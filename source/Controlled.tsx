33a36032dd2a61d22cc556fbfb928910b61c7439
Robert W. Pearce <me@robertwpearce.com>
2021-12-11 13:06:21 -0500

    fragment for forwarding children

--- source/Controlled.tsx
+++ source/Controlled.tsx
@@ -1,8 +1,8 @@
-import { memo } from 'react'
+import React, { memo } from 'react'
 import Base, { BaseProps } from './Base'
 
 // If we're not in a browser environment,
 // there is no need to zoom images.
 export default typeof window !== 'undefined'
   ? memo(Base)
-  : (props: BaseProps) => props.children
+  : (props: BaseProps) => <>{props.children}</>
